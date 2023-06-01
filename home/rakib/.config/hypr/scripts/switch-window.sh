0a577e2d501a4f43c375623fa308f0dfdb83034b
rakibdev <rakibdev01@gmail.com>
2023-06-01 16:43:04 +0600

    remove unused hyprland scripts

--- home/rakib/.config/hypr/scripts/switch-window.sh
+++
@@ -1,8 +0,0 @@
-window_count=$(hyprctl workspaces | pcregrep -o1 "windows: (\d+)")
-fullscreen=$(hyprctl workspaces | pcregrep -o1 "hasfullscreen: (\d+)")
-if [ $window_count -gt 2 ]; then
-  hyprctl dispatch fullscreen 1
-else
-  hyprctl dispatch focusurgentorlast ""
-  if [ $fullscreen == 0 ]; then hyprctl dispatch fullscreen 1; fi
-fi
