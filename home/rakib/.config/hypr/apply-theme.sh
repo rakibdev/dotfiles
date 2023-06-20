d37beb931283f36676e588158813b769a206a56a
rakibdev <rakibdev01@gmail.com>
2023-06-20 23:43:59 +0600

    fluent icon theme

--- home/rakib/.config/hypr/apply-theme.sh
+++ home/rakib/.config/hypr/apply-theme.sh
@@ -1,14 +1,15 @@
+gnome_schema="org.gnome.desktop.interface"
+gtk_theme="MonoThemeDark"
+icon_theme="Fluent-dark"
+cursor_theme="Bibata-Modern-Classic"
+font_name="Google Sans"
+
 # force reload
 gsettings set "$gnome_schema" gtk-theme ""
 gsettings set "$gnome_schema" icon-theme ""
 gsettings set "$gnome_schema" cursor-theme ""
 sleep 1
 
-gnome_schema="org.gnome.desktop.interface"
-gtk_theme="MonoThemeDark"
-icon_theme="Adwaita"
-cursor_theme="Bibata-Modern-Classic"
-font_name="Google Sans"
 gsettings set "$gnome_schema" gtk-theme "$gtk_theme"
 gsettings set "$gnome_schema" icon-theme "$icon_theme"
 gsettings set "$gnome_schema" cursor-theme "$cursor_theme"
