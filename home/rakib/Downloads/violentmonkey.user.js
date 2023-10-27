072bc9c960271a68e7fb76ca665d123200d9e1c0
rakibdev <rakibdev01@gmail.com>
2023-10-28 05:36:38 +0600

    add ublock filters

--- home/rakib/Downloads/violentmonkey.user.js
+++ home/rakib/Downloads/violentmonkey.user.js
@@ -1,18 +1,19 @@
 // ==UserScript==
 // @name        Custom
-// @version     1.0
 // @match       https://*/*
 // @grant       GM_xmlhttpRequest
+// @grant       GM_getValue
+// @grant       GM_setValue
 // @run-at      document-start
 // ==/UserScript==
 
-const applyTheme = theme => {
+const applyCss = theme => {
   let css = `
   ::-webkit-scrollbar {
     width: 8px;
     height: 8px;
   }
-  
+
   ::-webkit-scrollbar-thumb {
     background-color: ${theme.primary_surface_4};
     border-radius: 30px;
@@ -31,17 +32,17 @@ const applyTheme = theme => {
     .FalWJb /* related searches rich */ {
       background-color: ${theme.primary_surface} !important;
     }
-  
+
     .RNNXgb, /* search */
     .aajZCb, /* suggestion */
     .k8XOCe /* related searches chips */ {
       background-color: ${theme.primary_surface_2} !important;
     }
-  
+
     a {
       color: ${theme.primary_40} !important;
     }
-  
+
     #sfooter {
       display: none;
     }`
@@ -54,18 +55,53 @@ const applyTheme = theme => {
       --yt-spec-base-background: ${theme.primary_surface} !important;
       --yt-spec-additive-background: ${theme.primary_surface_2} !important; /* chips */
       --yt-spec-menu-background: ${theme.primary_surface_2} !important;
+      --paper-dialog-background-color: ${theme.primary_surface_2} !important; /* save to playlist dialog */
     }
-  
+
     .ytd-searchbox {
       --ytd-searchbox-background: ${theme.primary_surface_2};
       --ytd-searchbox-legacy-button-color: ${theme.primary_surface_2};
       border: 0 !important;
     }
-  
+
     /* disable player ambient mode */
     #cinematics canvas {
       display: none;
     }`
+  }
+
+  if (location.href.includes('music.youtube.com')) {
+    css += `
+    html {
+      --ytmusic-general-background-c: ${theme.primary_surface} !important; /* navigation rail, appbar */
+      --ytmusic-color-black1: ${theme.primary_surface_2} !important; /* bottom player, context menu */
+    }
+
+    body {
+      background-color: ${theme.primary_surface};
+    }
+
+    ytmusic-search-box[is-bauhaus-sidenav-enabled][is-mobile-view][has-query] {
+      --ytmusic-search-background: ${theme.primary_surface_2} !important;
+    }
+
+    ytmusic-tabs.stuck {
+      background-color: ${theme.primary_surface} !important;
+    }
+
+    ytmusic-responsive-list-item-renderer {
+      --ytmusic-list-item-height: 100px !important;
+      --ytmusic-responsive-list-item-thumbnail-size: 100px !important;
+    }
+    ytmusic-player-queue-item {
+      --ytmusic-list-item-height: 100px !important;
+      --ytmusic-player-queue-item-thumbnail-size: 100px !important;
+    }
+
+    ytmusic-play-button-renderer[size=MUSIC_PLAY_BUTTON_SIZE_SMALL] {
+      --ytmusic-play-button-size: 100px !important;
+    }
+    `
   }
 
   if (location.href.includes('github.com')) {
@@ -74,7 +110,7 @@ const applyTheme = theme => {
       height: 100%;
       --color-canvas-default: ${theme.primary_surface} !important; /* fallback body */
     }
-  
+
     body {
       --color-canvas-inset: ${theme.primary_surface}; /* header */
       --color-canvas-subtle: ${theme.primary_surface}; /* comemnt header */
@@ -99,7 +135,7 @@ const applyTheme = theme => {
       --theme-content-background-color: ${theme.primary_surface} !important;
       --highlight-bg: ${theme.primary_surface_2} !important; /* code highlight */
     }
-  
+
     #question-header .question-hyperlink {
       color: ${theme.primary_40} !important;
     }
@@ -132,18 +168,38 @@ const applyTheme = theme => {
     `
   }
 
+  if (location.href.includes('discord.com/channels')) {
+    css += `
+    :root {
+      --primary-600: ${theme.primary_surface} !important; /* content */
+      --primary-630: ${theme.primary_surface} !important; /* sidebar, discord loader */
+      --primary-660: ${theme.primary_surface} !important; /* sidebar bottom bar */
+      --primary-700: ${theme.primary_surface} !important; /* navigation rail */
+      --channeltextarea-background: ${theme.primary_surface_2}; /* message field */
+      --background-modifier-selected: ${theme.primary_surface_2} !important; /* list selected */
+      --green-360: ${theme.primary_40} !important; /* icon */
+      --green-430: ${theme.primary_40} !important; /* button */
+      --brand-experiment: ${theme.primary_40} !important; /* unread message alert */
+    }
+    `
+  }
+
   const style = document.createElement('style')
   style.textContent = css
   document.head.appendChild(style)
 }
+
+const getTheme = () => JSON.parse(GM_getValue('theme'))
+const cachedTheme = getTheme()
+if (cachedTheme) applyCss(cachedTheme)
 
 const themeFile = 'file:///home/rakib/.cache/system-ui/theme.json'
 GM_xmlhttpRequest({
   method: 'GET',
   url: themeFile,
   onload: response => {
-    const theme = JSON.parse(response.responseText)
-    applyTheme(theme)
+    GM_setValue('theme', response.responseText)
+    if (!cachedTheme) applyCss(getTheme())
   },
   onerror() {
     console.error(`Failed to read ${themeFile}`)
@@ -159,9 +215,7 @@ const googleSearchResultRedirect = () => {
   // disable link tracking redirection
   // https://github.com/raffaeleflorio/anti_rwt/blob/master/anti_rwt.js
   Object.defineProperty(window, 'rwt', {
-    value: (...args) => {
-      return true
-    },
+    value: () => true,
     writable: false,
     configurable: false,
     enumerable: false
