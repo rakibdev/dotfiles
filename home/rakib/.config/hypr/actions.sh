1350741ab5df4fd415e24e47dbc08f2f1f7c18b0
rakibdev <rakibdev01@gmail.com>
2024-01-18 09:09:48 +0600

    remvoe eww

---
+++ home/rakib/.config/hypr/actions.sh
@@ -0,0 +1,23 @@
+switch_window() {
+    local workspace=$(hyprctl activeworkspace)
+    local windows_count=$(echo "$workspace" | pcregrep -o1 "windows: (\d+)")
+    local fullscreen=$(echo "$workspace" | pcregrep -o1 "hasfullscreen: (\d+)")
+    if [ $windows_count -gt 2 ]; then
+        hyprctl dispatch fullscreen 1
+    else
+        hyprctl dispatch focusurgentorlast ""
+        if [ $fullscreen == 0 ]; then hyprctl dispatch fullscreen 1; fi
+    fi
+}
+
+close_tab_or_window() {
+    local window=$(hyprctl activewindow | pcregrep -o1 "class: (\w+)")
+    if [[ $window == "foot" || $window == "thunar" ]]; then
+        hyprctl dispatch killactive
+    else
+        wtype -M ctrl w -m ctrl
+    fi
+}
+
+if [ "$1" == "switch-window" ]; then switch_window; fi
+if [ "$1" == "close-tab-or-window" ]; then close_tab_or_window; fi
