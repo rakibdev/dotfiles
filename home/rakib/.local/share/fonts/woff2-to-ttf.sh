d6a251dba0d07892249a36d0f5cb45743809409f
rakibdev <rakibdev01@gmail.com>
2024-02-03 17:45:07 +0600

    use material symbols package

--- home/rakib/.local/share/fonts/woff2-to-ttf.sh
+++ home/rakib/.local/share/fonts/woff2-to-ttf.sh
@@ -1,4 +1,5 @@
 # open https://fonts.googleapis.com/css2?family=Google+Sans and download latin variant.
+# woff2_decompress requires woff2 package.
 
 for file in *.woff2; do
     woff2_decompress $file
