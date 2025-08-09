00903f817f690de5b1410829e831e8ff2a54453e
rakibdev <rakibdev01@gmail.com>
2025-08-09 19:32:55 +0600

    scripts

--- Downloads/scripts/violentmonkey/redirector.js
+++
@@ -1,9 +0,0 @@
-// ==UserScript==
-// @name        Redirector
-// @match        *://*.reddit.com/*
-// @match        *://*.quora.com/*
-// @run-at      document-start
-// ==/UserScript==
-
-if (location.hostname.includes('reddit.com')) location.hostname = 'redlib.catsarch.com'
-if (location.hostname.includes('quora.com')) location.hostname = 'qt.bloat.cat'
