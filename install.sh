e389536e49ccf3f9505fd8113db19ab2731f05ac
rakibdev <rakibdev01@gmail.com>
2026-04-09 10:54:28 +0600

    kernel install

--- install.sh
+++ install.sh
@@ -1,6 +1,3 @@
-stow --restow Downloads --target="$HOME/Downloads"
 stow --restow home --target="$HOME"
 stow --restow etc --target=/etc
-
-
 ln -sf "$(pwd)/material-gtk" "$HOME/.local/share/themes/material-gtk"
