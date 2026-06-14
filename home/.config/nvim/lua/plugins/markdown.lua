64680b668c9a62c747a4411fcc9daec91f2bd64f
rakibdev <rakibdev01@gmail.com>
2026-06-14 22:44:14 +0600

    markdown

---
+++ home/.config/nvim/lua/plugins/markdown.lua
@@ -0,0 +1,21 @@
+return {
+	'MeanderingProgrammer/render-markdown.nvim',
+	ft = { 'markdown' },
+	opts = {
+		-- fixes double click toggling raw text
+		render_modes = { 'n', 's', 'S', '\19' },
+		latex = { enabled = false },
+		sign = { enabled = false },
+		anti_conceal = { enabled = false },
+		heading = {
+			sign = false,
+			backgrounds = {},
+      -- inline removes left gap in heading
+			position = 'inline',     }
+		code = { sign = false },
+		link = {
+			image = false,
+			image_custom = false,
+		},
+	},
+}
