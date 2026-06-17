4681ac402b94ea610a520f50c86a0dff1078f729
rakibdev <rakibdev01@gmail.com>
2026-06-18 00:42:40 +0600

    modified dot

--- home/.config/nvim/lua/plugins/format.lua
+++ home/.config/nvim/lua/plugins/format.lua
@@ -1,18 +1,19 @@
 return {
-  'stevearc/conform.nvim',
-  event = 'BufWritePre',
-  opts = {
-    formatters_by_ft = {
-      javascript      = { 'oxfmt' },
-      javascriptreact = { 'oxfmt' },
-      typescript      = { 'oxfmt' },
-      typescriptreact = { 'oxfmt' },
-      json            = { 'oxfmt' },
-      jsonc           = { 'oxfmt' },
-      c               = { 'clang_format' },
-      cpp             = { 'clang_format' },
-      lua             = { 'stylua' },
-    },
-    format_on_save = {},
-  },
+	'stevearc/conform.nvim',
+	event = 'BufWritePre',
+	opts = {
+		formatters_by_ft = {
+			javascript = { 'oxfmt' },
+			javascriptreact = { 'oxfmt' },
+			typescript = { 'oxfmt' },
+			typescriptreact = { 'oxfmt' },
+			json = { 'oxfmt' },
+			jsonc = { 'oxfmt' },
+			c = { 'clang_format' },
+			cpp = { 'clang_format' },
+			lua = { 'stylua' },
+		},
+		-- fixes statusbar keeps showing file modified dot
+		format_on_save = { undojoin = true },
+	},
 }
